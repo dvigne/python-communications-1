3df1b0c2d179c59e4fa95f7a438faff2e768c5c0
Derick Vigne <derickvigne@me.com>
2019-02-15 21:35:55 -0500

    --wip-- [skip ci]

--- Digital_Communication_Project1.py
+++ Digital_Communication_Project1.py
@@ -22,7 +22,7 @@ def bac(txBits,p1, p2): #simulates a binary symmetric channel with transition pr
     flips[x < p1] = True
     rxBits[txBits == 0] = np.logical_xor(rxBits[txBits == 0], flips)
 
-    flips = np.zeros_like(txBits[txBits > 0], dtype='bool')
+    flips = np.zeros_like(rxBits[txBits > 0], dtype='bool')
     x = np.random.rand(flips.shape[0])
     flips[x < p2] = True
     rxBits[txBits > 0] = np.logical_xor(rxBits[txBits > 0], flips)
